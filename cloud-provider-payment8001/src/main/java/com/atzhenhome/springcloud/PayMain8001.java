c2e76295c8b66fde104c300a1eee3bea2a11a014
wuchengzhen <wuchengzhen@hzzl.net>
2020-03-21 17:48:38 +0800

    payment8001入驻Eureka

--- cloud-provider-payment8001/src/main/java/com/atzhenhome/springcloud/PayMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atzhenhome/springcloud/PayMain8001.java
@@ -2,6 +2,7 @@ package com.atzhenhome.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author:Chengzhen
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @ver:1.0
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PayMain8001 {
 
     public static void main(String[] args) {
